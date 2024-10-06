e137d56caa261c1a824cb8e4afde1342b309229a
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2024-10-06 14:28:11 -0700

    changed to white bg

--- app/(dashboard)/map/page.tsx
+++ app/(dashboard)/map/page.tsx
@@ -5,14 +5,14 @@ import UsMap from "../../../components/ui/UsMap";
 export default function Map() {
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-black">
       <h1 className="text-4xl font-bold mb-8">United States Map</h1>
       <div className="flex space-x-4 mb-8">
       </div>
       <div className="w-full max-w-4xl">
-          <div className="text-center">
-            <UsMap />
-          </div>
+        <div className="text-center">
+          <UsMap />
+        </div>
       </div>
     </div>
   );
